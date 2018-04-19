73beca5d78a39e15cf7e49b7a7851a3ae502715c
denebola213 <regulus@live.jp>
2018-04-19 21:50:41 +0900

    bug fix

--- script/post-receive.sh
+++ script/post-receive.sh
@@ -3,15 +3,12 @@
 cd $(dirname $(readlink -f $0))
 cd ../
 
-bundle install --path vendor/bundler > ./log/bundle.log
+bundle install --path vendor/bundle > ./log/bundle.log
 
 # logディレクトリがなければ作る
 if test ! -d 'log' ;then
   mkdir 'log'
 fi
-
-#Shell Script のパーミッション変更
-chmod -R 755 script
 
 /bin/sh ./script/cronsetting.sh
 #/bin/sh ./script/restart_commandbot.sh
