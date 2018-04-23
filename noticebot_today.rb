7febc9c55bad50a0f50433237af58b7905d329e0
denebola213 <regulus@live.jp>
2018-04-24 01:18:12 +0900

    bug fix

---
+++ noticebot_today.rb
@@ -0,0 +1,26 @@
+require 'bundler'
+require 'date'
+require 'open-uri'
+Bundler.require
+Dotenv.load
+
+module QKbot
+  def self.load file, foldername
+    Dir[File.dirname(file) + "/#{foldername}/*.rb"].each do |file|
+      require_relative file
+    end
+  end
+  
+  #STDOUT in debug. Log File in release
+  # debug: STDOUT
+  # release: './log/QKbot.log'
+  LOG = Logger.new(STDOUT)
+end
+
+QKbot.load(__FILE__, "lib")
+
+QKbot::DB.crawle
+
+tomorrow = Date.today
+QKbot::Twitter.tweet_info(tomorrow)
+QKbot::Discord.post_info(tomorrow)
