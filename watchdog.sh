b9c558205c395c7c8b3c8610a2fb663a885529af
denebola213 <info@denebola.info>
2018-08-08 00:32:58 +0900

    [fix] カレントディレクトリからの相対パスに

--- watchdog.sh
+++ watchdog.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 DOCKER_PS=`docker ps -a | grep "qkbot" | grep "Exited (1)" | sed -e "s/[^a-zA-Z_0-9]\+/#/g" | sed -e "s/^[a-zA-Z_0-9#]\+\(qkbot_[a-zA-Z_0-9]\+\)$/\1/g"`
-WEBHOOKS_URL=`cat .env | grep "WEBHOOKS" | sed -e 's/^[^"]\+"\([^"]\+\)\+"$/\1/g'`
+ENV_FILE="`dirname $0`/.env"
+WEBHOOKS_URL=`cat $ENV_FILE | grep "WEBHOOKS" | sed -e 's/^[^"]\+"\([^"]\+\)\+"$/\1/g'`
 
 for container in $DOCKER_PS; do
     docker logs -t $container >$container.log 2>&1
